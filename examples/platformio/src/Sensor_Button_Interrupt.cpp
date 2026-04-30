14fa85890921c469c354e663928db376f1dc9127
Gary Thomas <gary@mlbassoc.com>
2026-04-30 11:38:50 -0600

    Add Sensor-Button(interrupt)

---
+++ examples/platformio/src/Sensor_Button_Interrupt.cpp
@@ -0,0 +1,36 @@
+#if defined(SENSOR_BUTTON_INTERRUPT)
+#include "unihiker_k10.h"
+
+void onButtonAPressed();
+void onButtonBPressed();
+void onButtonABPressed();
+
+UNIHIKER_K10 k10;
+uint8_t screen_dir = 2;
+
+void onButtonAPressed() {
+    k10.canvas->canvasRectangle(10, 100, 110, 100, 0xFF6666, 0x0000FF, true);
+    k10.canvas->updateCanvas();
+}
+void onButtonBPressed() {
+    k10.canvas->canvasCircle(180, 150, 50, 0xFF6666, 0x0000FF, true);
+    k10.canvas->updateCanvas();
+}
+void onButtonABPressed() {
+    k10.canvas->canvasClear();
+    k10.canvas->updateCanvas();
+}
+
+void setup() {
+    Serial.begin(115200);
+    sleep(3);
+    Serial.println("\n\n\nUNIHIKER K10 Sensor Button Interrupt Example");
+    k10.begin();
+    k10.buttonA->setPressedCallback(onButtonAPressed);
+    k10.initScreen(screen_dir);
+    k10.creatCanvas();
+    k10.buttonB->setPressedCallback(onButtonBPressed);
+    k10.buttonAB->setPressedCallback(onButtonABPressed);
+}
+void loop() {}
+#endif
